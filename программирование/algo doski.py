3680d9af0f09a73eb1072edcc35d0cb827d91b9f
vintdime <nocom0540@gmail.com>
2026-09-01 14:02:47 +0300

    прога

---
+++ программирование/algo doski.py
@@ -0,0 +1,11 @@
+import os
+import math
+price=(int(40))
+print("Мы предоставляем доски 12х150см, толщиной 40мм.")
+print("Для установки забора введите свои желаемые параметры")
+#представим что участок абсолютно квадратный
+area=int(input("Введите площадь своего участкa:"))
+side=math.sqrt(area)
+perimeter=side * 4
+round
+print (side)
