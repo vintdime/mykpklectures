244004c40ba6f09ce01193c8da4e010db63f32ae
vintdime <nocom0540@gmail.com>
2026-09-01 16:16:43 +0300

    да

--- программирование/algo doski.py
+++ программирование/algo doski.py
@@ -1,11 +1,42 @@
 import os
 import math
-price=(int(40))
-print("Мы предоставляем доски 12х150см, толщиной 40мм.")
+print("Мы предоставляем доски 12х150см, толщиной 20,30 и 40мм.")
 print("Для установки забора введите свои желаемые параметры")
+price20=int(100)
+price30=int(120)
+price40=int(140)
 #представим что участок абсолютно квадратный
-area=int(input("Введите площадь своего участкa:"))
+area=int(input("Введите площадь своего участкa в метрах квадратных:"))
 side=math.sqrt(area)
+#используется округление для упрощения задач
+side=round(side)
 perimeter=side * 4
-round
-print (side)
+# Одна доска-0.12м в ширину. Это главное что нам нужно. Для измерения сколько нужно досок для забора нужно умножить perimeter на 0.12
+kalitka=float(input("Введите ширину вашей калитки в метрах:"))
+perimeter=perimeter-kalitka
+amountoflogs=perimeter/0.12
+amountoflogs=round(amountoflogs)
+#для запаса +1 доска к округленному
+amountoflogs=amountoflogs+1
+print("Пожалуйста, выберите вид досок:")
+print("1-20 мм")
+print("2-20 мм")
+print("3-30 мм")
+while True:
+    choice=input("Выберите номер:")
+
+    if choice in ["1", "2", "3"]:
+         break
+
+    print("Неправильный номер")
+
+if choice=="1":
+    price=price20
+elif choice=="2":
+    price=price30
+elif choice=="3":
+    price=price40
+total_price = amountoflogs * price
+print(f"Вам нужно {amountoflogs}досок, что будет стоить {total_price} рублей. По {price} за каждую")
+username=os.getlogin()
+print(f"Хорошего дня,{username}")
