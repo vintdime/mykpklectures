7f16cc4c25c421a4cd5fa07bb3813a50c195e395
vintdime <nocom0540@gmail.com>
2026-09-08 14:44:20 +0300

    1

--- mykpklectures/программирование/4sep/123.py
+++
@@ -1,7 +0,0 @@
-n = int(input())
-s = 0
-
-for i in range(1, n + 1):
-    s += i * i
-
-print("S =", s)
--- mykpklectures/программирование/4sep/rectangle.py
+++
@@ -1,4 +0,0 @@
-length=float(input("Введите длину прямоугольника:"))
-width=float(input("Введите ширину прямоугольника:"))
-landmass=length * width
-print (f"Площадь равна {landmass}")
